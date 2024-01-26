898b5cafa38c6d6aa267b4f1795f07b115eaca18
Lucas Cruz <lukkasnascimento2014@gmail.com>
2024-01-26 12:46:43 -0300

    Utilizando massa de dados

---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,25 @@
+///<reference types= "cypress" />
+
+
+describe('Funcionalidade Pagina de Produtos', () => {
+
+    beforeEach('', () => {
+
+        cy.visit("http://lojaebac.ebaconline.art.br/produtos")
+
+    });
+
+
+    it.only('Deve selecionar um produto da lista', () => {
+        var quantidade = 2
+
+        cy.get('[class="product-block grid"] ').contains("Ariel Roll Sleeve Sweatshirt").click()
+        cy.get(".button-variable-item-M").click()
+        cy.get(".button-variable-item-Purple").click()
+        cy.get(".input-text").clear().type(quantidade)
+        cy.get(".single_add_to_cart_button").click()
+
+        cy.get(".dropdown-toggle > .mini-cart-items").should("contain" , quantidade)
+        cy.get('.woocommerce-message').should("contain" , "2 x Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.")
+    });
+});
